ea8032cc0efb4ddb3020f26ae4dc8000fb45f56a
Евгений Антонюк <evgenijantonyuk@gmail.com>
2025-09-28 09:12:25 +0700

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+
+//     ===================================== hamburger-menu ================================
+const openMenuBtn = document.querySelector('#burger')
+const mobileMenu = document.querySelector('.header__nav-mobile')
+const closeMenu = document.querySelector('.header__burger-btn')
+
+openMenuBtn.addEventListener('click', function () {
+    console.log('click')
+    mobileMenu.classList.toggle('active')
+    closeMenu.classList.toggle('active')
+})
+// =============================
 const detailsButton = document.getElementById('detailsId')
 const details = document.querySelector('.details')
 const iconClose = document.querySelector('.icon__close')
